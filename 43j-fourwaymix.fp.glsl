5599f09492483611b5a40d859dda97a59ca083ab
Avneesh Sarwate <avneeshsarwate@gmail.com>
2018-06-12 17:31:32 +0200

    basic splicing and scrolling working

--- 43j-fourwaymix.fp.glsl
+++ 43j-fourwaymix.fp.glsl
@@ -2,7 +2,6 @@ uniform vec4 a;
 uniform vec4 b;
 uniform vec4 c;
 uniform vec4 d;
-uniform vec2 resolution;
 uniform float time;
 
 // define our varying texture coordinates
@@ -17,16 +16,22 @@ uniform sampler2DRect tex1;
 uniform sampler2DRect tex2;
 uniform sampler2DRect tex3;
 
+
+
 void main (void)
 {
+    vec2 resolution = vec2(320., 240);
 	// sample our textures
-	vec4 input0 = texture2DRect(tex0, texcoord0);
-	vec4 input1 = texture2DRect(tex1, texcoord1);
+	vec4 input0 = texture2DRect(tex0, mod(texcoord0 + time*20., resolution));
+	vec4 input1 = texture2DRect(tex1, mod(texcoord1 + time*20., resolution));
 	vec4 input2 = texture2DRect(tex2, texcoord2);
 	vec4 input3 = texture2DRect(tex3, texcoord3);
 	
+    vec4 c;
+    if(texcoord0.y > resolution.y / 2.) c = input0;
+    else c = input1;
 	// perform our calculation and write our data to the fragment color
-	gl_FragColor = a*input0 + b*input1 + c*input2 + d*input3;
-    gl_FragColor = vec4(texcoord0.x/320., texcoord0.y/240., mod(time, 1.0), 0.);
+	gl_FragColor = c;
+    // gl_FragColor = vec4(texcoord0.x/320., texcoord0.y/240., mod(time, 1.0), 0.);
 	
 }
